b502aeb7248efb57c5ee001371e046b6e64f4658
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-08-26 14:50:26 +0530

    dataType

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,6 +2,8 @@ const accountId=12345;
 let accountEmail="dkpriy8@gmail.com"
 var accountPassword="12345"
 accountCity="Bangalore"
+let accountState;  // we can declare variable without assigning value
+
 
 //accountId=23456;   // we cannot change the value of const variableoz
  console.log(accountId);
@@ -13,3 +15,11 @@ console.log(accountPassword);
 accountCity="Mumbai"  // we can change the value of variable without var,let,const
 console.log(accountCity);
 console.table([accountId,accountEmail,accountPassword,accountCity]);    
+
+/*
+prefer not to use var 
+beacause of issue in block scope and function scope
+use let and const.
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
