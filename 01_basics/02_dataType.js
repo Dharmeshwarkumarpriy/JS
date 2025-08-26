3390e98b321096355c64aac6289f10dd69c6f88a
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-08-26 15:04:18 +0530

    next

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,5 +1,5 @@
 "use strict"; //treat all js code as newer version
-//alert("Hello World!");
+// alert("Hello World!");
 
-//alert(3 + 1 + 2); // we can also do math inside
-
+// alert(3 + 1 + 2) // we can also do math inside
+// code readability should be high.
