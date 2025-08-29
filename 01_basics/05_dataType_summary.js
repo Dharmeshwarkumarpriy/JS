ca9d373d51c096647f0a83decec02ec5079cfef7
Dharmeshwarkumarpriy <dkpriy8402@gmail.com>
2025-08-29 21:06:55 +0530

    up

---
+++ 01_basics/05_dataType_summary.js
@@ -0,0 +1,25 @@
+// primetive
+
+// 7 type : string, number, boolean, null, undefined, symbol,bigInt
+
+// reference (Non primitive)
+// array, objects, functions
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+let useId=undefined
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+const bigNumber=34354454334433443n
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj{
+    name:'dhrmay',
+    age: 22,
+
+
+}
